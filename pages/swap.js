e2ba8fd603970b855e866ed7a2e09dfc35719161
j2k <designerbjk@gmail.com>
2022-07-09 15:31:03 +0900

    calculate Purple amount

--- pages/swap.js
+++ pages/swap.js
@@ -1,12 +1,13 @@
-import ConnectWallet2 from "./connectWallet2";
 import { useState } from "react";
+import ConnectWallet2 from "./connectWallet2";
 
-const PurpleCounter = () => {
-  const [count, setCount] = useState(0);
-  return <input className="{counter}"></input>;
-};
+const CalcPurple = () => {
+  const [message, setMessage] = useState();
 
-export default function swap() {
+  const handleChange = (event) => {
+    setMessage(event.target.value);
+  };
+
   return (
     <div className="flex justify-center pt-10">
       <div className="bg-gradient-to-r from-blue-800 via-violet-900 to-purple-800 card w-96 shadow-xl">
@@ -14,24 +15,33 @@ export default function swap() {
           <h2 className="card-title text-slate-300">From</h2>
           <p className="">USDC</p>
           <input
+            className="input input-bordered input-primary w-full max-w-xs text-right text-xl h-18"
             type="number"
-            pattern="[+-]?\d+(?:[.,]\d+)?"
+            id="message"
+            name="message"
+            onChange={handleChange}
+            value={message}
             placeholder="0.0"
-            min="0"
-            className="input input-bordered input-primary w-full max-w-xs text-right text-xl h-18 "
           />
+
           <h2 className="card-title pt-10 text-slate-300">To</h2>
           <p className="">Trust Seed Token</p>
+
           <input
-            type="number"
-            placeholder="0.0"
-            min="0"
             className="input input-bordered input-primary w-full max-w-xs text-right text-xl h-18"
+            type="number"
+            id="message"
+            name="message"
+            value={message * 20}
+            placeholder="0.0"
           />
+
           <p className="text-center pt-3 pb-3">1 PURPLE = 0.05 USDC</p>
           <ConnectWallet2 />
         </div>
       </div>
     </div>
   );
-}
+};
+
+export default CalcPurple;
