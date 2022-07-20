d0db50cc9dff9ff0571de3cab2ce11bb369e9ee9
j2k <designerbjk@gmail.com>
2022-07-20 19:16:13 +0900

    changed theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,16 @@
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}"],
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  // daisyUI config (optional)
+  daisyui: {
+    styled: true,
+    themes: ["dark"],
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+    lightTheme: "dark",
+  },
 };
